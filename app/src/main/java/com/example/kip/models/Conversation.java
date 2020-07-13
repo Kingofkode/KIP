e86f3b1eb0f62c29f36dcf0c1b2f158ced52db0c
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-13 13:16:35 -0700

    Created Message and Conversation models

---
+++ app/src/main/java/com/example/kip/models/Conversation.java
@@ -0,0 +1,20 @@
+package com.example.kip.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+@ParseClassName("Message")
+public class Conversation extends ParseObject {
+  public static final String KEY_MEMBER_IDS = "memberIDs";
+  public static final String KEY_LAST_MESSAGE = "lastMessage";
+
+  public List<ParseUser> getMembers() { return (List<ParseUser>) get(KEY_MEMBER_IDS); }
+  public void setMembers(List<ParseUser> members) { put(KEY_MEMBER_IDS, members); }
+
+  public Message getLastMessage() { return (Message) get(KEY_LAST_MESSAGE); }
+  public void setLastMessage(Message lastMessage) { put(KEY_LAST_MESSAGE, lastMessage); }
+
+}
---
+++ app/src/main/java/com/example/kip/models/Message.java
@@ -0,0 +1,28 @@
+package com.example.kip.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Message")
+public class Message extends ParseObject {
+
+  public static final String KEY_BODY = "body";
+  public static final String KEY_SENDER_ID = "senderID";
+  public static final String KEY_IMAGE = "image";
+  public static final String KEY_CONVERSATION_ID = "conversationID";
+
+  public String getBody() { return getString(KEY_BODY); }
+  public void setBody(String body) { put(KEY_BODY, body); }
+
+  public ParseUser getSender() { return getParseUser(KEY_SENDER_ID); }
+  public void setSender(ParseUser sender) {put(KEY_SENDER_ID, sender); }
+
+  public ParseFile getImage() { return getParseFile(KEY_IMAGE); }
+  public void setImage(ParseFile parseFile) { put(KEY_IMAGE, parseFile); }
+
+  public Conversation getConversation() { return (Conversation) get(KEY_CONVERSATION_ID); }
+  public void setConversation(Conversation conversation) { put(KEY_CONVERSATION_ID, conversation); }
+
+}
