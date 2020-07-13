2f4d6e26f7942901ec2708213560cc6bc45ebce4
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-13 12:49:57 -0700

    Initialized Parse.

---
+++ app/src/main/java/com/example/kip/ParseApplication.java
@@ -0,0 +1,28 @@
+package com.example.kip;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    registerParseModels();
+
+    // set applicationId, and server server based on the values in the Heroku settings.
+    // clientKey is not needed unless explicitly configured
+    // any network interceptors must be added with the Configuration Builder given this syntax
+    Parse.initialize(new Parse.Configuration.Builder(this)
+      .applicationId("kip-fbu") // should correspond to APP_ID env variable
+      .clientKey(null)  // set explicitly unless clientKey is explicitly configured on Parse server
+      .server("https://kip-fbu.herokuapp.com/parse").build());
+  }
+
+  private void registerParseModels() {
+    // ParseObject.registerSubclass(Post.class);
+  }
+}
