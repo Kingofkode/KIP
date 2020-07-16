60be1dd2045d4bcc9a1bf29c1b48fa7d546a23dd
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-16 15:04:30 -0700

    Added recycler view to MessageActivity, its layouts and adapter.

--- app/src/main/java/com/example/kip/activities/MessageActivity.java
+++ app/src/main/java/com/example/kip/activities/MessageActivity.java
@@ -5,20 +5,37 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.kip.adapters.MessagesAdapter;
 import com.example.kip.databinding.ActivityMessageBinding;
+import com.example.kip.models.Message;
+
+import java.util.ArrayList;
 
 public class MessageActivity extends AppCompatActivity {
 
   private static final String TAG = "MessageActivity";
 
   ActivityMessageBinding binding;
+  MessagesAdapter adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     binding = ActivityMessageBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
+
+    // Setup recycler view
+    ArrayList<Message> messages = new ArrayList<>();
+    Message message1 = new Message();
+    message1.setBody("Hi Isaiah!");
+    messages.add(message1);
+
+    adapter = new MessagesAdapter(this, messages);
+
+    binding.rvMessages.setAdapter(adapter);
+    binding.rvMessages.setLayoutManager(new LinearLayoutManager(this));
 
   }
 
--- app/src/main/java/com/example/kip/adapters/MessagesAdapter.java
+++ app/src/main/java/com/example/kip/adapters/MessagesAdapter.java
@@ -1,12 +1,15 @@
 package com.example.kip.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.kip.R;
+import com.example.kip.databinding.ItemMessageBinding;
 import com.example.kip.models.Message;
 
 import java.util.List;
@@ -24,23 +27,28 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
   @NonNull
   @Override
   public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return null;
+    View view = LayoutInflater.from(context).inflate(R.layout.item_message, parent, false);
+    return new ViewHolder(view);
   }
 
   @Override
   public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    holder.binding.tvBody.setText(messages.get(position).getBody());
 
   }
 
   @Override
   public int getItemCount() {
-    return 0;
+    return messages.size();
   }
 
   class ViewHolder extends RecyclerView.ViewHolder {
 
+    ItemMessageBinding binding;
+
     public ViewHolder(@NonNull View itemView) {
       super(itemView);
+      binding = ItemMessageBinding.bind(itemView);
     }
   }
 }
