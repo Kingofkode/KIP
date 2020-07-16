ca2233f70e9ee96b007e943b8d2fbd6f1fb2ff13
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-16 10:04:33 -0700

    Filled out ProfileActivity.

--- app/src/main/java/com/example/kip/activities/ProfileActivity.java
+++ app/src/main/java/com/example/kip/activities/ProfileActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.example.kip.databinding.ActivityMainBinding;
 import com.example.kip.databinding.ActivityProfileBinding;
 
@@ -19,7 +20,15 @@ public class ProfileActivity extends AppCompatActivity {
 
     binding = ActivityProfileBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
-
+    populateWithSampleData();
 
   }
+  private void populateWithSampleData() {
+    // TODO: Replace with actual profile image
+    Glide.with(this)
+      .load("https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/01/15/zuckprofpic.jpg?w968h681")
+      .circleCrop()
+      .into(binding.ivProfile);
+  }
 }
+
