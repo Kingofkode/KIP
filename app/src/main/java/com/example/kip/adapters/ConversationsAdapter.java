fe91c42d4e214844c9a5b4f7a62b62e1c2ca5164
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-16 09:49:02 -0700

    Finalized conversation layout.

--- app/src/main/java/com/example/kip/adapters/ConversationsAdapter.java
+++ app/src/main/java/com/example/kip/adapters/ConversationsAdapter.java
@@ -9,8 +9,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.kip.R;
 import com.example.kip.activities.MessageActivity;
+import com.example.kip.databinding.ItemConversationBinding;
 
 public class ConversationsAdapter extends RecyclerView.Adapter<ConversationsAdapter.ViewHolder> {
 
@@ -29,7 +31,11 @@ public class ConversationsAdapter extends RecyclerView.Adapter<ConversationsAdap
 
   @Override
   public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+    // TODO: Load appropriate profile image
+    Glide.with(context)
+      .load("https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/01/15/zuckprofpic.jpg?w968h681")
+      .circleCrop()
+      .into(holder.binding.ivProfile);
   }
 
   @Override
@@ -39,8 +45,11 @@ public class ConversationsAdapter extends RecyclerView.Adapter<ConversationsAdap
 
   class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+    ItemConversationBinding binding;
+
     public ViewHolder(@NonNull View itemView) {
       super(itemView);
+      binding = ItemConversationBinding.bind(itemView);
       itemView.setOnClickListener(this);
     }
 
