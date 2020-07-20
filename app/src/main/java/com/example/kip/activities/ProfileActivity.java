f0ac8675c64c17be825b8aa71f48c1697c2fd8fd
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-20 11:55:07 -0700

    Extracted photo related funtionality to PhotoActivity class which ProfileActivity now inherits from. Could be useful by MessageActivity if photos are supported in messages.

--- app/src/main/java/com/example/kip/activities/ProfileActivity.java
+++ app/src/main/java/com/example/kip/activities/ProfileActivity.java
@@ -27,7 +27,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends PhotoActivity {
 
   public static final String KEY_FRIEND_IDS = "friendIDs";
 
@@ -85,49 +85,6 @@ public class ProfileActivity extends AppCompatActivity {
     builder.show();
   }
 
-  public final String APP_TAG = "MyCustomApp";
-  public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-  public String photoFileName = "photo.jpg";
-  File photoFile;
-
-  public void launchCamera() {
-    // create Intent to take a picture and return control to the calling application
-    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    // Create a File reference for future access
-    photoFile = getTakenPhotoFileUri(photoFileName);
-
-    // wrap File object into a content provider
-    // required for API >= 24
-    // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-    Uri fileProvider = FileProvider.getUriForFile(ProfileActivity.this, "com.KIP.fileprovider", photoFile);
-    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-    // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-    // So as long as the result is not null, it's safe to use the intent.
-    if (intent.resolveActivity(getPackageManager()) != null) {
-      // Start the image capture intent to take photo
-      startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-    }
-  }
-
-  // Returns the File for a photo stored on disk given the fileName
-  public File getTakenPhotoFileUri(String fileName) {
-    // Get safe storage directory for photos
-    // Use `getExternalFilesDir` on Context to access package-specific directories.
-    // This way, we don't need to request external read/write runtime permissions.
-    File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
-
-    // Create the storage directory if it does not exist
-    if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-      Log.d(APP_TAG, "failed to create directory");
-    }
-
-    // Return the file target for the photo based on filename
-    File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-    return file;
-  }
-
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
@@ -154,42 +111,6 @@ public class ProfileActivity extends AppCompatActivity {
       binding.ivProfile.setImageBitmap(selectedImage);
     }
 
-  }
-
-  // PICK_PHOTO_CODE is a constant integer
-  public final static int PICK_PHOTO_CODE = 1046;
-
-  // Trigger gallery selection for a photo
-  public void pickPhoto() {
-    // Create intent for picking a photo from the gallery
-    Intent intent = new Intent(Intent.ACTION_PICK,
-      MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-    // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-    // So as long as the result is not null, it's safe to use the intent.
-    if (intent.resolveActivity(getPackageManager()) != null) {
-      // Bring up gallery to select a photo
-      startActivityForResult(intent, PICK_PHOTO_CODE);
-    }
-  }
-
-  public Bitmap loadChosenPhotoFromUri(Uri photoUri) {
-    Bitmap image = null;
-    try {
-      // check version of Android on device
-      if(Build.VERSION.SDK_INT > 27){
-        // on newer versions of Android, use the new decodeBitmap method
-        ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), photoUri);
-        image = ImageDecoder.decodeBitmap(source);
-      } else {
-        // support older versions of Android by using getBitmap
-        image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
-      }
-    } catch (IOException e) {
-      Log.e(TAG, "loadFromUri: ", e);
-      e.printStackTrace();
-    }
-    return image;
   }
 
 }
